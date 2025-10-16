5efea9e63e75e4256ad4e4f5b0ffc4d2bb5466de
anybrian <brian.reed@anywhere.re>
2025-10-16 01:22:24 -0400

    build cleanup

--- .scripts/build.ts
+++ .scripts/build.ts
@@ -1,25 +1,8 @@
 import { execSync } from 'child_process';
 
-const { DEV_GIT_TOKEN, MODE } = process.env;
+const { MODE } = process.env;
 
 console.log(`Building in ${MODE} mode`);
-
-if (MODE !== 'production') {
-    let branch = '';
-    if (MODE === 'development') branch = 'dev';
-    else if (MODE === 'test') branch = 'test';
-
-    console.log(
-        `Installing ${branch} branch of bspk-ui package and building with token: "${DEV_GIT_TOKEN?.substring(0, 4)}"...\n\n`,
-    );
-
-    execSync(
-        `npm install https://${DEV_GIT_TOKEN}@github.com/Anywhererealestate/bspk-ui#${branch} && npm explore @bspk/ui -- npm run build`,
-        {
-            stdio: 'inherit',
-        },
-    );
-}
 
 execSync(`npm run meta && vite-node ./.scripts/search-index.ts && vite build && cp dist/index.html dist/404.html`, {
     stdio: 'inherit',
